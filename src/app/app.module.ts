e4c21335638f38c0eff02e7a342e61a76fe28cc7
raksit31667 <raksit.m@ku.th>
2021-01-26 11:11:11 +0700

    🔧 configure translate module lazy loading via HTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {AngularFireModule} from "@angular/fire";
 import {AngularFireMessagingModule} from "@angular/fire/messaging";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -14,14 +22,23 @@ import {AngularFireMessagingModule} from "@angular/fire/messaging";
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp({
-      apiKey: '<your-key>',
-      authDomain: '<your-project-authdomain>',
-      databaseURL: '<your-database-URL>',
-      projectId: '<your-project-id>',
-      storageBucket: '<your-storage-bucket>',
-      messagingSenderId: '<your-messaging-sender-id>'
+      apiKey: "AIzaSyC3ZcPKewDVs59zQJuH9RNxzdGLZsAHh4Q",
+      authDomain: "example-angular-order.firebaseapp.com",
+      projectId: "example-angular-order",
+      storageBucket: "example-angular-order.appspot.com",
+      messagingSenderId: "448486945963",
+      appId: "1:448486945963:web:9eb0f728bcd2d189e2f3ee"
     }),
-    AngularFireMessagingModule
+    AngularFireMessagingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
