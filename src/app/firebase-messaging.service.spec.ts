757605db7ebf50aaab5742edb46cd69f1018181c
raksit31667 <raksit.m@ku.th>
2021-01-26 16:53:12 +0700

    ✅ fix dependency injection error on FirebaseMessagingService test

--- src/app/firebase-messaging.service.spec.ts
+++ src/app/firebase-messaging.service.spec.ts
@@ -1,12 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FirebaseMessagingService } from './firebase-messaging.service';
+import {AngularFireMessaging} from "@angular/fire/messaging";
 
 describe('FirebaseMessagingService', () => {
   let service: FirebaseMessagingService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: AngularFireMessaging, useValue: {
+            requestToken: {
+              subscribe: jasmine.createSpy('subscribe')
+            },
+            messages: {
+              subscribe: jasmine.createSpy('subscribe')
+            }
+          }
+        },
+      ]
+    });
     service = TestBed.inject(FirebaseMessagingService);
   });
 
